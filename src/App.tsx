9553d725326024c9ecef3851d20aaf29dd403bb0
Marcos Acosta <macosta1625@gmail.com>
2024-12-24 00:42:56 -0500

    Fix scrolling on navigate

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,12 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Homepage from "./components/Homepage";
 import { Post } from "./components/Post";
 import { POSTS } from "./logic/posts-util";
+import ScrollToTop from "./components/ScrollToTop";
 
 function App() {
   return (
     <BrowserRouter>
+      <ScrollToTop />
       <Routes>
         <Route path="/" element={<Homepage />} />
         {POSTS.map((postData) => (
