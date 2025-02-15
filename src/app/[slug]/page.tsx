b78b8a133fab6bdf086775f8f6c2299fa8e08674
Marcos Acosta <macosta1625@gmail.com>
2025-02-15 14:37:43 -0500

    Add header

--- src/app/[slug]/page.tsx
+++ src/app/[slug]/page.tsx
@@ -8,9 +8,15 @@ import styles from "./blog.module.css";
 import { directive } from "micromark-extension-directive";
 import { directiveFromMarkdown } from "mdast-util-directive";
 import { useParams } from "next/navigation";
+import { combineClasses } from "@/util";
+import Image from "next/image";
+import { xanh_mono } from "../page";
+import Link from "next/link";
 
 export default function Article() {
   const [content, setContent] = useState(null as Node | null);
+  const [scrollY, setScrollY] = useState(0);
+  const [scrollingUp, setScrollingUp] = useState(true);
   const paramsNew = useParams();
   const [annotationId, setAnnotationId] = useState(null as string | null);
 
@@ -28,13 +34,50 @@ export default function Article() {
     fetchPosts();
   }, []);
 
-  console.log(content);
+  const updateScrollDirection = () => {
+    setScrollY(window.scrollY);
+    if (window.scrollY > scrollY) {
+      setScrollingUp(false);
+    } else {
+      setScrollingUp(true);
+    }
+  };
+
+  useEffect(() => {
+    addEventListener("scroll", updateScrollDirection);
+    return () => {
+      removeEventListener("scroll", updateScrollDirection);
+    };
+  }, [scrollY]);
 
   return content ? (
     <div
       className={styles.cancelClickScreen}
       onClick={() => setAnnotationId(null)}
     >
+      <div
+        className={combineClasses(
+          styles.headerContainer,
+          !scrollingUp && styles.hide
+        )}
+      >
+        <div className={styles.headerContent}>
+          <Link href="/" className={styles.titleContainer}>
+            <Image
+              className={styles.detectiveImage}
+              src="/images/detective.png"
+              height={40}
+              width={20}
+              alt="A little detective figure"
+            />
+            <div
+              className={combineClasses(styles.headerText, xanh_mono.className)}
+            >
+              world's worst detective
+            </div>
+          </Link>
+        </div>
+      </div>
       <div className={styles.blogContainer}>
         <div className={styles.textContainer}>
           <AstRenderer
