22fb4941c19920a75e3af7b0a167c08c6eadeb84
Marcos Acosta <macosta1625@gmail.com>
2025-02-15 19:20:45 -0500

    Add RSS feed (!)

---
+++ src/app/rss.xml/route.ts
@@ -0,0 +1,35 @@
+import { POSTS } from "@/posts";
+import RSS from "rss";
+
+export default async function generateRssFeed() {
+  const site_url = "https://worldsworstdetective.com";
+
+  const feedOptions = {
+    title: "World's Worst Dective",
+    description: "???",
+    site_url: site_url,
+    feed_url: `${site_url}/rss.xml`,
+    image_url: `${site_url}/detective.png`,
+  };
+
+  const feed = new RSS(feedOptions);
+
+  POSTS.map((post) => {
+    !post.hide &&
+      feed.item({
+        title: post.title,
+        description: post.description,
+        url: `${site_url}/${post.url}`,
+        date: post.datePublished,
+      });
+  });
+
+  return feed;
+}
+export async function GET(request: Request) {
+  const rss = await generateRssFeed();
+  const xml = rss.xml();
+  const headers = new Headers();
+  headers.append("Content-Type", "text/xml");
+  return new Response(xml, { headers: headers });
+}
--- src/posts.ts
+++ src/posts.ts
@@ -4,6 +4,7 @@ export interface PostData {
   title: string;
   url: string;
   datePublished: Date;
+  description: string;
   hide?: boolean;
 }
 
@@ -12,16 +13,22 @@ export const POSTS: PostData[] = [
     title: "No dumb questions: Why is math always right?",
     url: "formal-systems",
     datePublished: makeDateLocalTime(2024, 12, 23),
+    description:
+      "Math is often our source of truth. We use it to learn truths about the universe. But what do symbols on a page have to do with reality?",
   },
   {
     title: "Social media wasn't the final boss",
     url: "doomscrolling",
     datePublished: makeDateLocalTime(2025, 1, 22),
+    description:
+      "I used to think that quitting social media would solve all my problems, but turns out it's more nuanced. Who knew?",
   },
   {
     title: "No dumb questions: Are there unthinkable thoughts?",
     url: "unthinkable-thoughts",
     datePublished: makeDateLocalTime(2025, 2, 15),
     hide: true,
+    description:
+      "Starfish will never do math. For that matter, monkeys aren't that great at it either. Are there limits to humans' capacity for thought?",
   },
 ];
